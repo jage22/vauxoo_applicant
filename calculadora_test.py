ffe045cc0578f74ac73c0a88957a569ec10f3214
Jesus Alan Guzman Escalante <aguzman@vauxoo.com>
2014-08-13 20:24:03 -0500

    [ADD] [calculator_test.py] creacion del ejercicio, para testear la calculadora , excercise number six

---
+++ calculadora_test.py
@@ -0,0 +1,19 @@
+#importamos el archivo calculator.py
+from calculator import *
+
+#codigo para pedir al usuario que ingrese los numero
+a = int(raw_input("ingrese un numero:"))
+b = int(raw_input("ingrese un numero:"))
+c = int(raw_input("ingrese un numero:"))
+d = int(raw_input("ingrese un numero:"))
+
+
+obj = calculator_class()#instanciamos la clase del archivo calculator.py
+print obj.sumar(lista)#ejecutamos la funcion que contiene la clase
+
+
+        
+
+
+
+
